6ac7896776f06fcbdc9dec0076f913b7673eca7f
Angel3713 <bharat3073@gmail.com>
2017-11-25 00:22:46 +0530

    idea
    self

---
+++ src/components/todos-list-header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+export default class TodosList extends React.Component {
+    render() {
+        return (
+          
+                <thead>
+                    <tr>
+                        <th>Tasks</th>
+                        <th>Actions</th>
+                    </tr>
+
+                </thead>
+           
+        )
+    }
+
+}
